d30862b847462d3fef06d64238c26e82c9e1158f
Dan <dan@getabetterpic.com>
2024-03-15 06:52:48 -0400

    Add middlewares to app; turn confirm link into a tag

--- functions/api/[[route]].ts
+++ functions/api/[[route]].ts
@@ -1,11 +1,15 @@
 import { Hono } from 'hono';
-import { getCookie, setCookie } from 'hono/cookie';
 import { handle } from 'hono/cloudflare-pages';
+import { poweredBy } from 'hono/powered-by';
+import { secureHeaders } from 'hono/secure-headers';
+import { etag } from 'hono/etag';
 import { Bindings } from '../utils/bindings';
-import * as bcrypt from 'bcryptjs';
 import users from './users';
 
 const app = new Hono<{ Bindings: Bindings }>();
+app.use(poweredBy());
+app.use(secureHeaders());
+app.use(etag());
 
 app.get('/api', (c) => {
   return c.json('Hello World');
--- functions/utils/send-confirmation-email.ts
+++ functions/utils/send-confirmation-email.ts
@@ -2,13 +2,14 @@ export async function sendConfirmationEmail(
   user: { email: string; username: string | null; confirmation_token: string },
   apiKey: string,
 ) {
+  const confirmUrl = `https://landforturn.com/api/users/confirm?confirmation_token=${user.confirmation_token}`;
   const form = new FormData();
   form.append('from', 'noreply@mail.landforturn.com');
   form.append('to', user.email);
   form.append('subject', 'Confirm your email');
   form.append(
     'html',
-    `Click here to confirm your email: https://landforturn.com/api/users/confirm?confirmation_token=${user.confirmation_token}`,
+    `Click here to confirm your email: <a href="${confirmUrl}">${confirmUrl}</a>`,
   );
   return fetch(`https://api.mailgun.net/v3/mail.landforturn.com/messages`, {
     method: 'POST',
